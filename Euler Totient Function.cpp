ede805dc00a82607e8c9a73e5e374210e54f5017
shamim7999 <46122116+shamim7999@users.noreply.github.com>
2020-05-12 10:29:20 +0600

    Update Euler Totient Function.cpp

--- Euler Totient Function.cpp
+++ Euler Totient Function.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define ll long long
 #define ull unsigned long long
 #define ld long double
-#define mx 200005
+#define mx 2000000
 #define mod 1000000007
 #define fr first
 #define cti(a) (a-48)
@@ -20,53 +20,26 @@ using namespace std;
 #define check(n, pos) (n & (1<<pos))
 #define Set(n, pos) (n | (1<<pos))
 #define PI acos(-1)
-#define n 1000000
-ll phi[n+2], phi2[n+2];
 
-void ETF1()
+ll phi[mx+2];
+
+void ETF()
 {
-    for(ll i=1; i<=n; i++) phi[i]=i;
-    for(ll i=2; i<=n; i+=2){
-        if(phi[i] == i){
-            for(ll j=i; j<=n; j+=i) phi[j] = (phi[j]*(i-1))/i;
-        }
-    }
-    for(ll i=3; i<=n; i+=2){
-        if(phi[i] == i){
-            for(ll j=i; j<=n; j+=i) phi[j] = (phi[j]*(i-1))/i;
+   for(int i=2; i<=mx; i++)
+        phi[i] = i;
+
+    for(int i=2; i<=mx; i++)
+    {
+        if(phi[i]==i)
+        {
+            for(int j=i; j<=mx; j+=i)
+                phi[j]-=phi[j]/i;
         }
     }
 }
-/// Another Technique
-void ETF2()
-{
-    for (int i = 1; i <= n; i++) {
-		phi2[i] = i;
-	}
-
-	for (int i = 2; i <= n; i++) {
-		if (phi2[i] == i) {
-			phi2[i] = i - 1;
-			for (int j = 2 * i; j <= n; j += i) {
-				phi2[j] = (phi2[j] * (i - 1)) / i;
-			}
-		}
-	}
-}
-
 int main()
 {
    fast;
-   ETF1();
-   ETF2();
-
-   for(ll i=1; i<=n; i++){
-      if(phi[i] != phi2[i]){
-         cout << "Miss_Match found in phi[" << i << "] = " << phi[i] << " and phi2[" << i << "] = " << phi2[i] << "\n";
-         return 0;
-      }
-   }
-   cout << "All the elements Matched.\n";
-
+   ETF();
    return 0;
 }
