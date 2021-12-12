fdc32a6135c312dd71d5a49220dbde080ed344cf
shamim7999 <46122116+shamim7999@users.noreply.github.com>
2021-12-12 15:22:44 +0600

    Create Fenwick_Tree.cpp

---
+++ Fenwick_Tree.cpp
@@ -0,0 +1,29 @@
+namespace Fenwick_2D_tree
+{
+    int tree[mx][mx];
+    void update(int x, int y, int val) {
+          int y1;
+          while (x <= n) {
+            y1 = y;
+            while (y1 <= m) {
+              tree[x][y1] += val;
+              y1 += (y1 & -y1);
+            }
+            x += (x & -x);
+          }
+    }
+    int query(int x, int y) {
+          int y1;
+          int sum = 0;
+          while (x>0) {
+            y1 = y;
+            while (y1>0) {
+              sum+=tree[x][y1];
+              y1 -= (y1 & -y1);
+            }
+            x -= (x & -x);
+          }
+          return sum;
+    }
+    // cout << query(x2, y2) - query(x1-1, y2) - query(x2, y1-1) + query(x1-1, y1-1) << "\n";
+};
