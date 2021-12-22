44d43f6845db5cfaf0057fdf5dd6367d292f3c6a
shamim7999 <46122116+shamim7999@users.noreply.github.com>
2021-12-23 00:24:59 +0600

    Update Fenwick_Tree.cpp

--- Fenwick_Tree.cpp
+++ Fenwick_Tree.cpp
@@ -27,3 +27,46 @@ namespace Fenwick_2D_tree
     }
     // cout << query(x2, y2) - query(x1-1, y2) - query(x2, y1-1) + query(x1-1, y1-1) << "\n";
 };
+
+
+namespace Fenwick_1D_Tree
+{
+    int tree[mx];
+    void update(int pos)
+    {
+        if(pos<=0)
+            return;
+        //tem[pos]++;
+        while(pos<=n){
+            tree[pos]++;
+            pos+=(pos & -pos);
+        }
+    }
+
+    void remove(int pos)
+    {
+        if(pos <= 0)
+            return;
+        //tem[pos]++;
+        while(pos<=n){
+            tree[pos]--;
+            pos+=(pos & -pos);
+        }
+    }
+
+    int query(int pos)
+    {
+        int sum = 0;
+        while(pos>0){
+            sum+=tree[pos];
+            pos-=(pos & -pos);
+        }
+        return sum;
+    }
+
+    int res(int l, int r)
+    {
+        int a1 = query(r) - query(l-1);
+        return a1;
+    }
+};
