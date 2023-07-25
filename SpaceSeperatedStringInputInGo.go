590327b90e2d84dd724d599194ca083091e932c1
shamim7999 <46122116+shamim7999@users.noreply.github.com>
2023-07-25 12:17:07 +0600

    Create SpaceSeperatedStringInputInGo.go

---
+++ SpaceSeperatedStringInputInGo.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strings"
+	//"strconv"
+)
+
+func getline(s1 string) string{
+	in := bufio.NewReader(os.Stdin)
+	s1, _ = in.ReadString('\n')
+	s1 = strings.TrimSpace(s1)
+	return s1
+}
+
+func main() {
+	var s1, s2 string
+	s1 = getline(s1)
+	s2 = getline(s2)
+	s2 = s1 + " " + s2
+	fmt.Println(s2)
+}
