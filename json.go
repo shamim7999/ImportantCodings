5dfc7fcb35119edf7782207c1190efa8a2c1ac73
shamim7999 <46122116+shamim7999@users.noreply.github.com>
2023-07-25 14:49:30 +0600

    Create json.go

---
+++ json.go
@@ -0,0 +1,46 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type permissions map[string]bool // #3
+
+type hobbies[] string /// -----------> I added for understanding
+
+type user struct { // #1
+	Name        string      `json:"username"`
+	Password    string      `json:"-"`
+	Permissions permissions `json:"perms,omitempty"` // #6
+
+	// name        string // #1
+	// password    string // #1
+	// permissions // #3
+	Hobbies   hobbies		`json:"hobbies"`
+}
+
+func main() {
+	users := []user{ // #2
+		{"inanc", "1234", nil, nil},
+		{"god", "42", permissions{"admin": true}, nil},
+		{"devil", "66", permissions{"write": true, "read" : true}, hobbies{"Reading", "Writing", "Coding", "etc."}},
+	}
+
+	// out, err := json.Marshal(users) // #4
+	out, err := json.MarshalIndent(users, "", "\t") // #5
+	if err != nil {                                 // #4
+		fmt.Println(err)
+		return 
+	}
+
+	fmt.Println(string(out)) // #4
+}
